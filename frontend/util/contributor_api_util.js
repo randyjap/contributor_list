d482b8d99b8985317deaaa10648237544f7a6b5f
randyjap <randyjap@gmail.com>
2017-04-08 15:28:14 -0700

    add production url

--- frontend/util/contributor_api_util.js
+++ frontend/util/contributor_api_util.js
@@ -1,21 +1,21 @@
 export const getContributors = () => (
   $.ajax({
     method: 'GET',
-    url: 'http://localhost:3000/api/contributors/'
+    url: '/api/contributors/'
   })
 );
 
 export const deleteContributor = id => (
   $.ajax({
     type: 'DELETE',
-    url: `http://localhost:3000/api/contributors/${id}`
+    url: `/api/contributors/${id}`
   })
 );
 
 export const postContributors = contributors => (
   $.ajax({
     type: 'POST',
-    url: 'http://localhost:3000/api/contributors/',
+    url: '/api/contributors/',
     data: JSON.stringify(contributors),
     contentType: "application/json"
   })
